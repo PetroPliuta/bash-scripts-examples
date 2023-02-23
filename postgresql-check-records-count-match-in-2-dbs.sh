616afe0368337de7ee9fd96e8920710b3f0432bf
Petro Pliuta <42343532+PetroPliuta@users.noreply.github.com>
2023-02-23 10:34:48 +0200

    Update postgresql-check-records-count-match-in-2-dbs.sh

--- postgresql-check-records-count-match-in-2-dbs.sh
+++ postgresql-check-records-count-match-in-2-dbs.sh
@@ -8,18 +8,14 @@ green(){
     echo -e "${GREEN}${@}${NC}"
 }
 
-# Allows read tables list from cli. Will verify only these tables
-# Example: 
-#> bash postgresql-check-records-count-match-in-2-dbs.sh table1 table2
 tables="$@"
 
-PASS='' # database password. In this case it is similar for both databases 
-
+PASS=''
 SRC_HOST=''
 DST_HOST=''
 
+
 schema="public"
-# schema='other'
 
 echo "Schema: $schema"
 
@@ -28,30 +24,48 @@ tables_count_dst=$(PGPASSWORD=$PASS psql -U postgres -d postgres -h $DST_HOST -t
 
 echo Tables count: $tables_count_src, $tables_count_dst
 
-if [[ "$tables_count_dst" != "$tables_count_src" ]]; then
+if (( "$tables_count_dst" != "$tables_count_src" )); then
     red "tables count do not match"
     exit 1
 fi
 
-if [ "$#" -eq "0" ]; then
-    tables="$(PGPASSWORD=$PASS psql -U postgres -d postgres -h $SRC_HOST -t -c "SELECT table_name FROM information_schema.tables WHERE table_type = 'BASE TABLE' and table_schema='$schema';")"
+if (( "$#" == "0" )); then
+    tables="$(PGPASSWORD=$PASS psql -U postgres -d postgres -h $SRC_HOST -t -c "SELECT table_name FROM information_schema.tables WHERE table_type = 'BASE TABLE' and table_schema='$schema' ORDER BY table_name;")"
 fi
 
 fail="false"
 failed_tables=""
 counter=1
 for table in $tables; do
-    echo "($counter/$tables_count_src) $table: "
+    echo -n "($counter/$tables_count_src) $table: "
     src_records_count=$(PGPASSWORD=$PASS psql -U postgres -d postgres -h $SRC_HOST -t -c "SELECT count(*) FROM $schema.$table ;")
-    echo -n "$src_records_count"
+    # src_records_count="0"
+    echo -n "$src_records_count, "
     dst_records_count=$(PGPASSWORD=$PASS psql -U postgres -d postgres -h $DST_HOST -t -c "SELECT count(*) FROM $schema.$table ;")
-    echo "$dst_records_count"
+    echo -n "$dst_records_count - "
 
-    if [[ "$src_records_count" == "$dst_records_count" ]]; then
+    if (( "$src_records_count" == "$dst_records_count" )); then
         green OK
+        
+        # # CHECKSUM
+        # https://www.cybertec-postgresql.com/en/postgresql-creating-checksums-for-tables/
+        #
+        # echo -n "Checksum: src - "
+        # md5_src=$(PGPASSWORD=$PASS psql -U postgres -d postgres -h $SRC_HOST -t -c "SELECT md5_agg() WITHIN GROUP (ORDER BY $table) FROM $table;")
+        # echo -n "$md5_src"
+        # echo -n ", dst - "
+        # md5_dst=$(PGPASSWORD=$PASS psql -U postgres -d postgres -h $DST_HOST -t -c "SELECT md5_agg() WITHIN GROUP (ORDER BY $table) FROM $table;")
+        # echo -n "$md5_dst - "
+        # if [[ "$md5_src" == "$md5_dst" ]]; then
+        #     green OK
+        # else
+        #     red ERROR
+        #     fail="true"
+        #     failed_tables="$table $failed_tables"
+        # fi
+        # # CHECKSUM
     else
         red ERROR
-        # exit 1
         fail="true"
         failed_tables="$table $failed_tables"
     fi
